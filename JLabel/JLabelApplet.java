1b18796c6cd8c59266f0bbd954c440b7362c8a7e
cvp2004 <94306745+cvp2004@users.noreply.github.com>
2022-09-13 19:48:24 +0530

    Initial Commit

---
+++ JLabel/JLabelApplet.java
@@ -0,0 +1,34 @@
+/*
+    <applet code="JLabelApplet.class" width="300" height="300"></applet>
+*/
+
+/*
+    Name : Chaitanya Praskar
+    Id Code : 20CM004
+    Applet Program for JLabel & ImageIcon
+*/
+
+
+import java.awt.*;
+import javax.swing.JApplet;
+import javax.swing.*;
+
+public class JLabelApplet extends JApplet {
+
+    @Override
+    public void init() {
+
+        ImageIcon imageIcon = new ImageIcon("img.png.jpg");
+        JLabel imageLabel = new JLabel(imageIcon);
+        
+        imageLabel.setBounds(100, 0, 100, 100);
+
+        add(imageLabel);
+        
+
+        setSize(300, 300);
+        setLayout(null);
+
+    }
+
+}
